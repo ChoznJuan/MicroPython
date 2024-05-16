80c35984c6e895c316abdf015ad283e4b8e9da82
ChoznJuan <djg5826@gmail.com>
2024-05-16 17:39:45 -0400

    update display

--- cyd-clock/main.py
+++ cyd-clock/main.py
@@ -1,16 +1,20 @@
-from ili9341 import Display, color565
-from xpt2046 import Touch
-from machine import Pin, SPI, SDCard, ADC, idle
-import network
-import time
+#from ili9341 import Display, color565
+#from xpt2046 import Touch
+#from machine import Pin, SPI, SDCard, ADC, idle
+#import network
+#import time
 import urequests
-import secrets
+#import secrets
 import json
 import os
 import wifi_manager as wifimgr 
-import utime
-#import usocket as socket
+#import utime
+from cydr import CYD
 
+#Initialize display
+cyd = CYD()
+
+#Launch WiFi Manager
 wlan = wifimgr.get_connection()
 if wlan is None:
     print("Could not initialize the network connection.")
@@ -20,38 +24,16 @@ if wlan is None:
 
 # Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
 print("ESP OK")
-
-# Set up SPI for display
-# Baud rate of 80000000 seems about the max
-display_spi = SPI(1, baudrate=80000000, sck=Pin(14), mosi=Pin(13))
-
-# Set up display
-# The library needs a reset pin, which does not exist on this board
-display = Display(display_spi, dc=Pin(2), cs=Pin(15), rst=Pin(15))
-
-#Set up display backlight
-backlight = Pin(21, Pin.OUT)
-
-
-WHITE = color565(255, 255, 255)
-
-#connect()
-#display available networks
-#print(networks)
-
-
-
-
-
+cyd.rgb(cyd.BLUE)
 #Get weather information
 weather = urequests.get('http://api.openweathermap.org/data/2.5/weather?q=Greensboro,NC,US&units=imperial&appid='+secrets.api)
 data = weather.json()
 temp = data['main']
 disp = round(temp['temp'])
 print(disp)
-backlight.on()
+cyd.backlight.on()
 
 #display functions
-display.clear(color565(255, 255, 0))
-display.draw_text8x8(display.width //32,display.height //9,str(disp),WHITE,background=0)
+cyd.display.clear(cyd.YELLOW)
+cyd.display.draw_text8x8(cyd.display.width //32,cyd.display.height //9,str(disp),cyd.WHITE,background=0)
 
